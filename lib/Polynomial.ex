4a1008e3f36b144dbb653a896bf1337c9f715db2
MahathiVavilala <mahathivavilala97@gmail.com>
2017-11-05 13:21:34 +0530

    Modified Polynomial

--- lib/Polynomial.ex
+++ lib/Polynomial.ex
@@ -1,19 +1,39 @@
-
 defmodule Polynomial do
-   def polyadd(a, b) do
+  def main(a, b) do
      lena = Enum.count(a)
      lenb = Enum.count(b)
-     list =
+     condition(a, b,lena, lenb)
+  end
+  
+  def take(c, len1, len2), do: Enum.take(c, len1-len2)
+  
+  def condition(a, b, lena, lenb) do
      cond  do
        lena > lenb -> take(a, lena, lenb)
        lenb > lena -> take(b, lenb, lena)
        true -> []
      end
-     append(list, a, b)
    end
-   def take(c, len1, len2), do: Enum.take(c, len1-len2)
-   def append(c, a, b) do
-        Enum.map(List.zip([Enum.reverse(a), Enum.reverse(b)]),fn({x, y}) -> x + y end) ++ c
+
+   def polyadd(a, b) do
+     list2 = main(a, b)
+     list1 = List.zip([Enum.reverse(a), Enum.reverse(b)])
+     add(list1, list2)
+   end
+   
+   def add(d, c) do
+        Enum.map(d, fn({x, y}) -> x + y end) ++ c
+        |> Enum.reverse
+   end
+
+   def polysub(a, b) do
+     list2 = main(a, b)
+     list1 = List.zip([Enum.reverse(a), Enum.reverse(b)])
+     sub(list1, list2)
+   end
+   
+   def sub(d, c) do
+        Enum.map(d,fn({x, y}) -> x - y end) ++ c
         |> Enum.reverse
    end
 end
