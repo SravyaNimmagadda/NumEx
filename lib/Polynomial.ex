3ad146a0187b11d36181d74f6c0d00c61dd424db
MahathiVavilala <mahathivavilala97@gmail.com>
2017-11-05 11:14:53 +0530

    Cleaned Polynomial.ex

--- lib/Polynomial.ex
+++ lib/Polynomial.ex
@@ -1,24 +1,17 @@
 defmodule Polynomial do
-
-  def polyadd(a, b) do
-    lena = Enum.count(a)
-    lenb = Enum.count(b)
-    c = []
-    
-    if(lena > lenb) do
-      c = a
-      |> Enum.take(lena - lenb)
-    end
-    if(lenb > lena) do
-      c = b
-      |> Enum.take(lenb - lena)
-    end
-
-    d = List.zip([Enum.reverse(a), Enum.reverse(b)])
-    |> Enum.map(fn({x, y}) -> x + y end)
-    d ++ c
-    |> Enum.reverse
-
-  end
-
+   def polyadd(a, b) do
+     lena = Enum.count(a)
+     lenb = Enum.count(b)
+     c = []
+     cond  do
+       lena > lenb -> c = take(a, lena, lenb)
+       lenb > lena -> c = take(b, lenb, lena)
+     end
+     append(c, a, b)
+   end
+   def take(c, l1, l2), do: Enum.take(c, l1-l2)
+   def append(c, a, b) do
+        Enum.map(List.zip([Enum.reverse(a), Enum.reverse(b)]),fn({x, y}) -> x + y end) ++ c
+        |> Enum.reverse
+   end
 end
