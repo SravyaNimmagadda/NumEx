bfb7308d0179cc5ce8c7df7d1895e01de7415d15
SravyaNimmagadda <nimmagadda.sravya97@gmail.com>
2017-12-28 21:57:37 +0530

    Polynomial Multiplication Added

--- lib/Polynomial.ex
+++ lib/Polynomial.ex
@@ -4,9 +4,9 @@ defmodule Polynomial do
      lenb = Enum.count(b)
      condition(a, b,lena, lenb)
   end
-  
+
   def take(c, len1, len2), do: Enum.take(c, len1-len2)
-  
+
   def condition(a, b, lena, lenb) do
      cond  do
        lena > lenb -> take(a, lena, lenb)
@@ -20,7 +20,7 @@ defmodule Polynomial do
      list1 = List.zip([Enum.reverse(a), Enum.reverse(b)])
      add(list1, list2)
    end
-   
+
    def add(d, c) do
         Enum.map(d, fn({x, y}) -> x + y end) ++ Enum.reverse(c)
         |> Enum.reverse
@@ -31,10 +31,23 @@ defmodule Polynomial do
      list1 = List.zip([Enum.reverse(a), Enum.reverse(b)])
      sub(list1, list2)
    end
-   
+
    def sub(d, c) do
         Enum.map(d,fn({x, y}) -> x - y end) ++ Enum.reverse(c)
         |> Enum.reverse
    end
-end
 
+   def polymul(a, b) do
+     lna = Enum.count(a) - 1
+     lnb = Enum.count(b) - 1
+     n = lna + lnb + 1
+     result = List.duplicate(0, n)
+
+     resultpoly = for i <- 0..lna, j <- 0..lnb, do: List.update_at(result, i+j ,&(&1 + (Enum.at(a, i) * Enum.at(b, j))))
+     resultpoly
+     |> Enum.zip
+     |> Enum.map(fn(x) -> Tuple.to_list(x) end)
+     |> Enum.map(fn(x) -> Enum.sum(x) end)
+
+    end
+end
