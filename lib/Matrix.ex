055fe98060d240c1074459163151fb385b9a85da
MahathiVavilala <mahathivavilala97@gmail.com>
2017-12-31 12:40:34 +0530

    Modified Docs

--- lib/Matrix.ex
+++ lib/Matrix.ex
@@ -1,10 +1,31 @@
 defmodule Matrix do
-  def max(h) when is_list(h) do
-    Enum.max(h)
+  def max(h) when is_list(h)do
+    Enum.map(h, fn x -> Enum.max(x) end)
   end
 
+#Yet to form the Matrix struc for rand
   def rand(row, col) do
-      r = List.duplicate(0, row * col)
-      Enum.map(r,fn(x) -> x = x + :rand.uniform(6) end)
+      List.duplicate(0,  col)
+      |> Enum.map(fn x -> x + :rand.uniform(6) end)
+  end
+ #Yet to form the Matrix struc for rand
+  def zeros(row, col) do
+    List.duplicate(0, row * col)
+  end
+
+  def ones(row, col) do
+    List.duplicate(1, row * col)
+  end
+
+  def rowcount(a = [h | _]) when is_list(h) do
+    Enum.count(a)
+  end
+
+  def columncount([first_row | _]) when is_list(first_row) do
+      Enum.count(first_row)
+  end
+
+  def size(a = [h | _]) when is_list(h) do
+    Enum.max([rowcount(a), columncount(a)])
   end
 end
