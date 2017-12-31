93ac181e5cd2fb2f39ff3e769bf1a26239ff9d18
SravyaNimmagadda <nimmagadda.sravya97@gmail.com>
2017-12-31 15:36:05 +0530

    Random Matrix generation

--- lib/Matrix.ex
+++ lib/Matrix.ex
@@ -5,10 +5,14 @@ defmodule Matrix do
 
 #Yet to form the Matrix struc for rand
   def rand(row, col) do
-      List.duplicate(0,  col)
-      |> Enum.map(fn x -> x + :rand.uniform(6) end)
+      List.duplicate(0, row)
+      |> Enum.map(fn x -> genrandrow(col) end)
   end
- #Yet to form the Matrix struc for rand
+
+  def genrandrow(col) do
+    List.duplicate(0, col)
+    |> Enum.map(fn x -> x + :rand.uniform(6) end)
+   end
   def zeros(row, col) do
     List.duplicate(0, row * col)
   end
