7e2d5678582f5feeb9efff2dd0f42276acdd9b8d
MahathiVavilala <mahathivavilala97@gmail.com>
2017-12-31 11:38:34 +0530

    Modified Math Functions

--- lib/MathematicalFunctions.ex
+++ lib/MathematicalFunctions.ex
@@ -3,4 +3,12 @@ defmodule MathematicalFunc do
     list
     |> Enum.map(fn x  -> :erlang.float_to_binary(:math.sin(x), decimals: 2)  end)
   end
+  def cos(list) do
+    list
+    |> Enum.map(fn x  -> :erlang.float_to_binary(:math.cos(x), decimals: 2)  end)
+  end
+  def tan(list) do
+    list
+    |> Enum.map(fn x  -> :erlang.float_to_binary(:math.tan(x), decimals: 2)  end)
+  end
 end
