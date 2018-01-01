3f85b7c5946a5b0ff040cb515c3e8d9a97bccb01
MahathiVavilala <mahathivavilala97@gmail.com>
2018-01-01 19:41:07 +0530

    Added test cases

--- lib/Counting.ex
+++ lib/Counting.ex
@@ -1,4 +1,12 @@
 defmodule Count do
+  @moduledoc """
+  A module to count non_zeros
+  """
+  @doc """
+  Takes an enumerable and counts all non-zero elements from it.
+      iex(1)> Count.count_nonzero([1,0,9,0,3,0,5])
+      4
+  """
   def count_nonzero(l) do
     Enum.count(l, fn(x)  -> x != 0 end)
   end
