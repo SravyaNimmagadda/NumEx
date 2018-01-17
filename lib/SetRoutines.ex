6c3410262f81ffc1f2b3f5b2ec11f048a0e33191
MahathiVavilala <mahathivavilala97@gmail.com>
2018-01-17 20:58:46 +0530

    solved SetRoutines and generated docs

--- lib/SetRoutines.ex
+++ lib/SetRoutines.ex
@@ -1,3 +1,15 @@
 defmodule SetRoutines do
-  
+  @moduledoc """
+  A module to perform some missing set operations
+  """
+  @doc """
+  Takes two enumerables, returns a MapSet by performing set exclusive-or on them, element-wise.
+  iex> SetRoutines.setxor1d([1,2,3,2,4],[2,3,5,7,5])
+  #MapSet<[1, 4, 5, 7]>
+  """
+  def setxor1d(arr1, arr2) do
+    x = MapSet.new(arr1)
+    y = MapSet.new(arr2)
+    MapSet.difference(MapSet.union(x, y), MapSet.intersection(x, y))
+  end
 end
