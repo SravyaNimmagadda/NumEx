82bb60a0a8d7298ece4678630341c7aeb1dff321
SravyaNimmagadda <nimmagadda.sravya97@gmail.com>
2018-01-18 07:05:42 +0530

    Added Docs for Polynomial

--- lib/Polynomial.ex
+++ lib/Polynomial.ex
@@ -1,5 +1,9 @@
 defmodule Polynomial do
-  
+
+  @moduledoc """
+  A module to perform various operations on Polynomials.
+  """
+
   defp main(a, b) do
      lena = Enum.count(a)
      lenb = Enum.count(b)
@@ -16,6 +20,15 @@ defmodule Polynomial do
      end
   end
 
+  @doc """
+  Returns the sum of two Polynomials
+
+
+      iex> Polynomial.polyadd([2, 3, 7], [1, 5])
+      [2, 4, 12]iex> Fraction.new(15)
+
+  """
+
   def polyadd(a, b) do
      list2 = main(a, b)
      list1 = List.zip([Enum.reverse(a), Enum.reverse(b)])
@@ -27,6 +40,14 @@ defmodule Polynomial do
         |> Enum.reverse
   end
 
+  @doc """
+  Returns the difference of two Polynomials
+
+
+      iex> Polynomial.polysub([2, 3, 7], [1, 5, 9])
+      [1, -2, -2]
+  """
+
   def polysub(a, b) do
      list2 = main(a, b)
      list1 = List.zip([Enum.reverse(a), Enum.reverse(b)])
@@ -37,6 +58,14 @@ defmodule Polynomial do
         Enum.map(d,fn({x, y}) -> x - y end) ++ Enum.reverse(c)
         |> Enum.reverse
   end
+
+  @doc """
+  Returns the product of two Polynomials
+
+
+      iex> Polynomial.polymul([5, 1, 3], [-1, 2])
+      [-5, 9, -1, 6]
+  """
 
   def polymul(a, b) do
      degree1 = Enum.count(a) - 1
@@ -51,8 +80,25 @@ defmodule Polynomial do
      |> Enum.map(fn(x) -> Enum.sum(x) end)
   end
 
+  @doc """
+  Returns the Polynomial multiplied with a given coefficient
+
+
+      iex> Polynomial.polymulx([4, 5, -2], 2)
+      [8, 10, -4]
+  """
+
   def polymulx(poly, x), do: Enum.map(poly, fn(m) -> m * x end)
 
+
+    @doc """
+    Returns the quotient and remainder of two Polynomials
+
+
+        iex> Polynomial.polydiv([4, 5, 2], [2, 1])
+        {[2, 1], [1]}
+
+    """
   def polydiv(_, []), do: raise ArgumentError, "denominator is zero"
   def polydiv(_, [0]), do: raise ArgumentError, "denominator is zero"
   def polydiv(f, g) when length(f) < length(g), do: {[0], f}
@@ -73,6 +119,15 @@ defmodule Polynomial do
     polydiv(g, q++[p], tl(temprem))
   end
 
+
+    @doc """
+    Returns the derivative of the given Polynomial
+
+
+        iex> Polynomial.polyder([6, -1, 3, 2])
+        [18, -2, 3]
+    """
+
   def polyder(a) do
     degree = Enum.count(a) - 1
     a |> Enum.with_index
@@ -80,24 +135,31 @@ defmodule Polynomial do
     |> Enum.take(degree)
   end
 
+  @doc """
+  Returns the the coefficients of the polynomial raised to the given power
+
+
+      iex> Polynomial.polypow([6, -1, 3], 2)
+      [36, 1, 9]
+
+  """
   def polypow(polynomial, power) do
     Enum.map(polynomial, fn(x) -> round(:math.pow(x, power)) end)
   end
 
-
-  def quadraticroots(a, b, c) do
-    d = b * b - 4 * a * c
-    a2 = a * 2
-    {r1, r2} =
-    cond do
-      d > 0 ->
-        sd = :math.sqrt(d)
-        {(- b + sd) / a2, (- b - sd) / a2}
-      d == 0 ->
-        {- b / a2, - b / a2}
-      true ->
-        sd = :math.sqrt(-d)
-        {(- b / a2) + (sd / a2), (- b / a2) - (sd / a2)}
-    end
-  end
+  #def quadraticroots(a, b, c) do
+  #  d = b * b - 4 * a * c
+  #  a2 = a * 2
+  #  {r1, r2} =
+  #  cond do
+  #    d > 0 ->
+  #      sd = :math.sqrt(d)
+  #      {(- b + sd) / a2, (- b - sd) / a2}
+  #    d == 0 ->
+  #      {- b / a2, - b / a2}
+  #    true ->
+  #      sd = :math.sqrt(-d)
+  #      {(- b / a2) + (sd / a2), (- b / a2) - (sd / a2)}
+  #  end
+  #end
 end
