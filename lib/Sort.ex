2a154680259d4c65fcc3a02690f57f17fa0d2cd5
SravyaNimmagadda <nimmagadda.sravya97@gmail.com>
2018-01-20 21:56:37 +0530

    Added LexSort

--- lib/Sort.ex
+++ lib/Sort.ex
@@ -13,4 +13,16 @@ defmodule Sort do
     {left, right} = Enum.partition(tail, fn(x) -> x < pivot end)
     quicksort(left) ++ [pivot] ++ quicksort(right)
   end
+
+  @doc """
+  Takes a two enumerables and performs Lexographic sort operations.
+
+      iex> Sort.lexsort([5, 3, 6, 8, 1], [0, 9, 4, 7, 2])
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    """
+    
+def lexsort(list1, list2) do
+    Enum.concat(list1, list2)
+    |> Enum.sort
+  end
 end
